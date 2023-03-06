88c18af98f90ae0a2b9a32a7975f6f210e55ba66
pdevda <paruldevda@gmail.com>
2023-03-05 21:40:18 -0800

    updated pom.xml for packaging and added Dockerfile

--- pingpong/src/main/java/com/games/pingpong/PingpongApplication.java
+++ pingpong/src/main/java/com/games/pingpong/PingpongApplication.java
@@ -3,6 +3,14 @@ package com.games.pingpong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * 
+ *  run this as java application, that brings up the app at localhost:8080 port
+ *  opening http:localhost:8080/api/ping - returns pong 
+ * @author pdevda
+ *
+ */
 @SpringBootApplication
 public class PingpongApplication {
 
